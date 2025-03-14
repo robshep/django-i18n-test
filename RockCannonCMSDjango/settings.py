76ccf58362f3a04b3b278c9f9f619d7664203089
Rob Shepherd <rob@evometric.com>
2025-03-14 00:30:29 +0000

    fix template

--- RockCannonCMSDjango/settings.py
+++ RockCannonCMSDjango/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-$v3f!=u**kl1uwwnyuiq(s9z%o!4tie#^urycrk0l5jjwh1ct)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '0.0.0.0', '127.0.0.1'
+]
 
 
 # Application definition
